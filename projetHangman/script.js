9a642da7bd5687ee17ac21d77d2b6b48f96b90cb
JennaLeeL <xjennyy@hotmail.ca>
2024-05-07 18:51:59 -0400

    New make it bigger and smaller button

--- projetHangman/script.js
+++ projetHangman/script.js
@@ -10,7 +10,7 @@ document.getElementById('start-btn').addEventListener('click', () => {
 });
 
 function initializeCastApi() {
-
+    // const sessionRequest = new chrome.cast.SessionRequest(applicationID);
     const sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
     // const sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media(applicationID));
     const apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
@@ -31,11 +31,10 @@ function sessionListener(newSession) {
 }
 function receiverListener(availability) {
     if (availability === chrome.cast.ReceiverAvailability.AVAILABLE) {
-        document.getElementById('start-btn').style.display = 'none';
-
+        document.getElementById('container').style.display = 'none';
     } else {
         document.getElementById('start-btn').style.display = 'none';
-        document.getElementById('container').style.display = 'none';
+        document.getElementById('container').style.display = 'block';
     }
 }
 
@@ -53,31 +52,49 @@ function handleClick() {
 
 }
 
-// CHANGER LA GROSSEUR DES BOUTONS
-
-const bigger = document.getElementById("bigger");
-
+// CHANGER VALEUR (SIZE) BOUTONS
 
 document.getElementById('bigger').addEventListener('click', function() {
-
     document.querySelectorAll('.alphabet').forEach(button => {
-        button.classList.replace('alphabet', 'alphabetPLUS');
+        let curW = parseFloat(window.getComputedStyle(button).width);
+        let curH = parseFloat(window.getComputedStyle(button).height);
+        let curSize = parseFloat(window.getComputedStyle(button).fontSize);
+        let newW = curW * 1.2;
+        let newH = curH * 1.2;
+        let newSize = curSize * 1.2;
+        button.style.width = newW + 'px';
+        button.style.height = newH + 'px';
+        button.style.fontSize = newSize + 'px';
     });
 });
 
-function handleClick() {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(button => {
-        button.addEventListener('click', handleClick, false);
+document.getElementById('smaller').addEventListener('click', function() {
+    document.querySelectorAll('.alphabet').forEach(button => {
+        let curW = parseFloat(window.getComputedStyle(button).width);
+        let curH = parseFloat(window.getComputedStyle(button).height);
+        let curSize = parseFloat(window.getComputedStyle(button).fontSize);
+        let newW = curW * 0.8;
+        let newH = curH * 0.8;
+        let newSize = curSize * 0.8;
+        button.style.width = newW + 'px';
+        button.style.height = newH + 'px';
+        button.style.fontSize = newSize + 'px';
     });
-        console.log(this.value);
-    }
-    const message = {
-        type: 'guess',
-        data: {
-            button :buttons
-        }
-    };
+});
+
+// function handleClick() {
+//     const buttons = document.querySelectorAll('button');
+//     buttons.forEach(button => {
+//         button.addEventListener('click', handleClick, false);
+//     });
+//         console.log(this.value);
+//     }
+//     const message = {
+//         type: 'guess',
+//         data: {
+//             button :buttons
+//         }
+//     };
 
 
 
